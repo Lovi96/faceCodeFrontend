66a105450c56b43b519365cb706cb86b62cf03f8
Ali <bodaleaa@gmail.com>
2017-07-19 12:25:47 +0200

    user class added

---
+++ src/app/classes/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  firstName: String;
+  lastName: String;
+  birthDate: number;
+  address: String;
+  city: String;
+  phone: String;
+  id: number;
+  email: String;
+  password: String;
+}
