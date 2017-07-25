959b98416f082e27e8e7b9c12cec9d6cd09ab7a0
Ali <bodaleaa@gmail.com>
2017-07-25 11:13:50 +0200

    login component prepared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
-
 import {AppComponent} from './app.component';
-import {RegPageComponent} from './reg-page/reg-page.component';
-
+import {RegPageComponent} from './components/reg-page/reg-page.component';
 import {FormsModule} from '@angular/forms';
 import {UserService} from './services/user.service'
+import {AppRoutingModule} from './app-routing.module';
+import {LoginComponent} from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RegPageComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- src/app/components/reg-page/reg-page.component.ts
+++ src/app/components/reg-page/reg-page.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {UserService} from '../services/user.service';
-import {User} from '../classes/User';
+import {UserService} from '../../services/user.service';
+import {User} from '../../classes/User';
 
 @Component({
   selector: 'app-reg-page',
