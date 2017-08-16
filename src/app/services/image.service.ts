41b327f9999e764893373f95f9503a7783e9dabf
Ali <bodaleaa@gmail.com>
2017-08-16 16:44:15 +0200

    Button texts fixed

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpWrapper} from "./http-wrapper.service";
 import {environment} from "../../environments/environment";
+import {Observable} from "rxjs/Observable";
+import {Result} from "../classes/Result";
+import {Response} from '@angular/http';
+
 
 @Injectable()
 export class ImageService {
 
-  constructor(private http: HttpWrapper) { }
+  constructor(private http: HttpWrapper) {
+  }
 
-  uploadProfileImage(): void {
+  uploadProfileImage(): Observable<Result> {
     let formData = new FormData();
     formData.append('image', document.getElementById('image')['files'][0]);
     let headers = new Headers();
-    this.http.post(environment.baseUrl + '/account/profileimage/upload', formData).subscribe();
+    return this.http.post(environment.baseUrl + '/account/profileimage/upload', formData).map((response: Response) =>
+      response.json());
   }
 
 }
