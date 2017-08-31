2a10dbe6c554367e0e44a5e159522a1c310f060c
Ali <bodaleaa@gmail.com>
2017-08-31 18:27:30 +0200

    login component cleanup

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,7 +6,6 @@ import {FCException} from '../../classes/FCException'
 import {Http} from '@angular/http';
 import {Router} from "@angular/router";
 import {MyGuard} from "../../guards/can-active.guard";
-import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'app-login',
@@ -22,26 +21,20 @@ export class LoginComponent implements OnInit {
 
   feedbackMessage: string;
 
-  constructor(private myGuard:MyGuard,private http: Http, private userService: UserService, private router: Router) {
+  constructor(private myGuard: MyGuard, private http: Http, private userService: UserService, private router: Router) {
   }
 
   ngOnInit(): void {
-    this.makeNavbarUnvisible();
     const token = this.myGuard.getToken();
-    if(token != null){
+    if (token != null) {
       this.router.navigate(['/profile']);
     }
   }
-
 
   logIn() {
     this.loginCredentail = new LoginCredentials(this.email, this.password);
     this.userService.logIn(this.loginCredentail).subscribe(x => {
       console.log(x);
-      // this.makeWelcomeGoAway();
-      // this.makeFooterGoAway();
-      // this.makeBackgroundPlain();
-      // this.makeLoginGoAway();
       if (x.exception) {
         if (x.exception.statusCode) {
           this.feedbackMessage = FCException.get(x.exception.statusCode);
@@ -56,15 +49,9 @@ export class LoginComponent implements OnInit {
         this.setUserId(x.payload.userID);
         this.feedbackMessage = 'Logged in successfully';
 
-        this.makeNavbarVisible();
         this.router.navigate(['/profile']);
-        this.makeBackgroundPlain();
       }
     })
-  }
-
-  makeNavbarVisible() {
-    document.getElementById("navBar").style.visibility = "visible";
   }
 
   setToken(token: number): void {
@@ -75,24 +62,5 @@ export class LoginComponent implements OnInit {
     localStorage.setItem('userID', userId.toString());
   }
 
-  makeNavbarUnvisible() {
-    document.getElementById("navBar").style.visibility = "hidden";
-  }
-
-  makeBackgroundPlain() {
-    document.getElementById("bodyOfPage").style.backgroundImage = "";
-  }
-
-  makeWelcomeGoAway() {
-    document.getElementById("goAway").innerHTML = "";
-  }
-
-  makeFooterGoAway() {
-    document.getElementById("footerElement").innerText = "";
-  }
-
-  makeLoginGoAway() {
-    document.getElementById("loginElement").innerHTML = "";
-  }
 
 }
